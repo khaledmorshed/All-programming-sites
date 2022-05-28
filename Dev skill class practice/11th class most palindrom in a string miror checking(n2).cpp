253ccb577c9b1fc45b8e569c8c2bc8dca068b0ae
khaledmorshed <s.m.morshed10@gmail.com>
2022-05-28 20:13:34 +0600

    most palindromic string iniside of a string with mirror effect wit n squre complexity

---
+++ Dev skill class practice/11th class most palindrom in a string miror checking(n2).cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str;
+int startIndex = 0, endIndex = 0;
+int mostPalindromic(){
+	int ans = 1;
+
+	for(int m=0; m<str.size(); m++){
+		for(int i=m-1, j=m+1; i>=0 && j<str.size(); i--, j++){
+			if(str[i] == str[j]){
+				int prevAns = ans;
+				ans = max(ans, j-i+1);
+				if(ans > prevAns){
+					startIndex = i;
+					endIndex = j;
+				}
+			}
+			else break;
+		}
+		for(int i=m, j=m+1; i>=0 && j<str.size(); i--, j++){
+			if(str[i] == str[j]){
+				int prevAns = ans;
+				ans = max(ans, j-i+1);
+				if(ans > prevAns){
+					startIndex = i;
+					endIndex = j;
+				}
+
+			}
+			else break;
+		}
+	}
+	return ans;
+}
+
+int main(){
+	cin>>str;
+	int resu = mostPalindromic();
+	cout<<"most palindrome string = ";
+	for(int i=startIndex; i<= endIndex; i++){
+		cout<<str[i];
+	}
+	cout<<" and length = "<<resu<<endl;
+}
